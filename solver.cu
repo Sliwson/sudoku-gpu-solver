e5373ea92a963676b19405e2d320fa64e2d23bd1
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2020-01-21 20:34:48 +0100

    Test shared memory

--- solver.cu
+++ solver.cu
@@ -26,15 +26,10 @@ namespace {
 	
 	u16* d_sudoku;
 	bool* d_propagated;
+	int* d_helperInt;
 }
 
-__device__ bool d_anyChanged;
-
-__global__ void testKernel()
-{
-    const unsigned int tid = threadIdx.x;
-    const unsigned int num_threads = blockDim.x;
-}
+__device__ bool d_kernelBool;
 
 __device__ void Clamp(int &val, const int &min, const int &max)
 {
@@ -49,104 +44,138 @@ __device__ bool IsPowerOfTwo(const u16 &x)
 	return x != 0 && (x & (x - 1)) == 0;
 }
 
-__global__ void Propagate(u16* d_mask, bool* d_propagated, int maskIdx)
+__device__ int CountOnes(const u16& x)
 {
+	int count = 0;
+	for (int i = 0; i <= 9; i++)
+	{
+		if (((x >> i) & 1) == 1)
+			count++;
+	}
+
+	return count;
+}
+
+__global__ void Propagate(u16* d_mask, bool* d_propagated)
+{
+	d_kernelBool = false;
+
     const int tid = threadIdx.x;
+	const int maskIdx = blockDim.x;
+
+	//set up startup point on matrix
+	d_mask += maskIdx;
+
 	if (tid >= 81)
 		return;
 
-	d_anyChanged = false;
-	if (d_propagated[tid] || !IsPowerOfTwo(d_mask[tid]))
-		return;
+	__shared__ u16 s[81];
+	s[tid] = d_mask[tid];
 
-	d_anyChanged = true;
-	d_propagated[tid] = true;
-	u16 propagationMask = 0x1ff & ~d_mask[tid];
-
-	//vertical
-	int pos = (tid + 9) % 81;
-
-	for (int i = 0; i < 8; i++)
+	if (!d_propagated[tid] && IsPowerOfTwo(s[tid]))
 	{
-		d_mask[pos] &= propagationMask;
-		pos = (pos + 9) % 81;
-	}
+		d_kernelBool = true;
+		d_propagated[tid] = true;
+		u16 propagationMask = 0x1ff & ~s[tid];
 
-	//horizontal
-	int left = (tid / 9) * 9;
-	int right = left + 8;
+		//vertical
+		int pos = (tid + 9) % 81;
 
-	pos = tid + 1;
-	if (pos > right)
-		pos = left;
-
-	for (int i = 0; i < 8; i++)
-	{
-		
-		d_mask[pos] &= propagationMask;
-		pos++;
-		if (pos > right)
-			pos = left;
-	}
-	
-	//in square
-	int sx = (tid % 9) / 3 * 3 + 1;
-	int sy = (tid / 27) * 3 + 1;
-	
-	int x = tid % 9;
-	int y = tid / 9;
-	for (int i = 0; i < 8; i++)
-	{
-		x++;
-		if (x > sx + 1)
+		for (int i = 0; i < 8; i++)
 		{
-			x = sx - 1;
-			y++;
-			if (y > sy + 1)
-				y = sy - 1;
+			s[pos] &= propagationMask;
+			pos = (pos + 9) % 81;
 		}
 
-		int p = 9 * y + x;
-		d_mask[p] &= propagationMask;
+		//horizontal
+		int left = (tid / 9) * 9;
+		int right = left + 8;
+
+		pos = tid + 1;
+		if (pos > right)
+			pos = left;
+
+		for (int i = 0; i < 8; i++)
+		{
+			
+			s[pos] &= propagationMask;
+			pos++;
+			if (pos > right)
+				pos = left;
+		}
+		
+		//in square
+		int sx = (tid % 9) / 3 * 3 + 1;
+		int sy = (tid / 27) * 3 + 1;
+		
+		int x = tid % 9;
+		int y = tid / 9;
+		for (int i = 0; i < 8; i++)
+		{
+			x++;
+			if (x > sx + 1)
+			{
+				x = sx - 1;
+				y++;
+				if (y > sy + 1)
+					y = sy - 1;
+			}
+
+			int p = 9 * y + x;
+			s[p] &= propagationMask;
+		}
 	}
+
+	d_mask[tid] = s[tid];
+}
+
+__global__ void FindLowest(u16* d_mask)
+{
+
 }
 
 void InitKernel()
 {
-	cudaMalloc(&d_propagated, 81 * sizeof(bool));
-	cudaMalloc(&d_sudoku, 81 * sizeof(u16));
+	cudaMalloc(&d_propagated, 81 * 1000 * sizeof(bool));
+	cudaMalloc(&d_sudoku, 81 * 1000 * sizeof(u16));
+	cudaMalloc(&d_helperInt, 2000 * sizeof(int));
 }
 
 void CleanKernel()
 {
 	cudaFree(d_sudoku);
 	cudaFree(d_propagated);
+	cudaFree(d_helperInt);
 }
 
 void runKernel(u16 sudoku[81], u16 result[81])
 {
 	u16 mask[81];
 	FillMask(sudoku, mask);
+	int activeMasks = 1;
 
 	cudaMemcpy(d_sudoku, mask, 81 * sizeof(u16), cudaMemcpyHostToDevice);
-	cudaMemset(d_propagated, false, 81 * sizeof(bool));
+	cudaMemset(d_propagated, false, 81 * 1000 * sizeof(bool));
+	cudaMemset(d_helperInt, 0, 2000 * sizeof(int));
 
-	// setup execution parameters
-    dim3  grid(1, 1, 1);
-    dim3  threads(128, 1, 1);
-
-    // execute the kernel
 	while (true)
 	{
-		Propagate << <grid, threads >> > (d_sudoku, d_propagated, 0);
-		bool anyChanged;
-		cudaMemcpyFromSymbol(&anyChanged, d_anyChanged, sizeof(anyChanged), 0, cudaMemcpyDeviceToHost);
-		if (!anyChanged)
-			break;
+		int activeMasksNew = activeMasks;
+
+		//propagate all sudokus
+		while (true)
+		{
+			Propagate <<<activeMasks, 128>> > (d_sudoku, d_propagated);
+			bool anyChanged;
+			cudaMemcpyFromSymbol(&anyChanged, d_kernelBool, sizeof(anyChanged), 0, cudaMemcpyDeviceToHost);
+			if (!anyChanged)
+				break;
+		}
+
+		//check for split
+		break;
 	}
-
+	
 	cudaMemcpy(mask, d_sudoku, 81 * sizeof(u16), cudaMemcpyDeviceToHost);
-
-
 	FillResult(mask, result);
 }
